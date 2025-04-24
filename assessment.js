2ddfb1a223594e2d93ef93b680f87518949e3707
Takumouse <1212oto.takuma@gmail.com>
2025-04-24 23:01:35 +0900

    Update assessment.js

--- assessment.js
+++ assessment.js
@@ -9,7 +9,8 @@ userNameInput.addEventListener('keydown', (event) => {
     if (event.code === 'Enter') {
         assessmentButton.dispatchEvent(new Event('click'));
     }
-});
+}
+
 
 assessmentButton.addEventListener('click', () => {
     const userName = userNameInput.value;
